d36a6e3bc6612efee1ab1aa5bdd519e42d2b46d3
Guido van de Riet <guidovdriet@gmail.com>
2020-02-29 10:15:00 +0100

    Set house rules page

--- src/templates/house-rules.js
+++ src/templates/house-rules.js
@@ -4,8 +4,15 @@ import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+export const HouseRulesTemplate = ({
+  title,
+  description,
+  content,
+  contentComponent
+}) => {
   const PageContent = contentComponent || Content;
+
+  console.log({ content });
 
   return (
     <section className="section section--gradient">
@@ -16,6 +23,7 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
               </h2>
+              <p>{description}</p>
               <PageContent className="content" content={content} />
             </div>
           </div>
@@ -25,18 +33,18 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   );
 };
 
-AboutPageTemplate.propTypes = {
+HouseRulesTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
   contentComponent: PropTypes.func
 };
 
-const AboutPage = ({ data }) => {
+const HouseRules = ({ data }) => {
   const { markdownRemark: post } = data;
 
   return (
     <Layout>
-      <AboutPageTemplate
+      <HouseRulesTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
@@ -45,18 +53,19 @@ const AboutPage = ({ data }) => {
   );
 };
 
-AboutPage.propTypes = {
+HouseRules.propTypes = {
   data: PropTypes.object.isRequired
 };
 
-export default AboutPage;
+export default HouseRules;
 
-export const aboutPageQuery = graphql`
-  query AboutPage($id: String!) {
+export const HouseRulesQuery = graphql`
+  query HouseRules($id: String!) {
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
         title
+        description
       }
     }
   }
